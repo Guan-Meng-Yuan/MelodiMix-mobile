ef91b1349aa4f270c186f461c75fb84eace9d829
guanmengyuan <guan.mengyuan@qq.com>
2023-12-26 11:25:11 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "@uni-helper/vite-plugin-uni-components";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [vue(), Components(), uni(), , AutoImport()],
 });
